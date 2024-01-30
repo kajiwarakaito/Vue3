b980df3e09e1fea64493a924cad8a5bbe5077789
kajiwarakaito <kajiwara.kaito@vice.co.jp>
2024-01-30 16:21:05 +0900

    add reactive-systemFolder

---
+++ reactive-system/src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+    import {ref} from "vue";
+
+    // 現在日時を取得
+    const now = new Date();
+    // 現在の時刻の文字列を取得
+    const nowStr = now.toLocaleTimeString();
+    // 現在の時刻文字列をテンプレート変数として用意
+    let timeStr = nowStr;
+    // 同じく現在の時刻文字列をテンプレート変数としてref()で用意
+    const timeStrRef = ref(nowStr);
+    // 新しい時刻に変更する関数
+
+    function changeTime(): void{
+        //現在日時を取得
+        const newTime = new Date();
+        // 現在の時刻の文字列を取得
+        const newTimeStr = newTime.toLocaleTimeString();
+        // 現在の時刻文字列をテンプレート変数timeStrに格納
+        timeStr = newTimeStr;
+        // 現在の時刻文字列をテンプレート変数timeStrRefに格納
+        timeStrRef.value = newTimeStr;
+        }
+        
+    //changeTime関数を1秒ごとに実行
+    setInterval(changeTime, 1000);
+
+    // const now = new Date();
+    // const nowStr = now.toLocaleTimeString();
+    // let timeStr = nowStr;
+    // const timeStrRef = ref(nowStr);
+
+    // function changeTime(): void{
+    //     const newTime = new Date();
+    //     const newTimeStr = newTime.toLocaleTimeString();
+    //     timeStr = newTimeStr;
+    //     timeStrRef.value = newTimeStr;
+    //     }
+        
+    // setInterval(changeTime, 1000);
+
+</script>
+
+<template>
+    <p>現在時刻: {{ timeStr }}</p>
+    <p>現在時刻(ref): {{ timeStrRef }}</p>
+</template>
